9231ec8325361f48b970e0e8d04490ce4faf0b46
Bit-Angel <luisangelromopadilla@gmail.com>
2022-05-30 11:42:27 -0500

    Interface Estudio añadida

---
+++ src/app/interfaces/estudios.interface.ts
@@ -0,0 +1,9 @@
+export default interface Estudio {
+    id?:string;
+    nombreEstudio:string;
+    idEstudio:string;
+    descripcion:string;
+    precio:number;
+    fecha:string;
+    hora:string;
+}
