35e3bb4d7616c59631980394298fba9f25a85e96
brayan <brayanvazq1608@gmail.com>
2022-05-31 22:32:36 -0500

    boton de iniciar/cerrar sesion

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,25 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { Auth } from '@angular/fire/auth';
+import { FirebaseService } from '../services/firebase.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.css']
+  styleUrls: ['./nav-bar.component.css'],
 })
 export class NavBarComponent implements OnInit {
+  bandera: boolean = true;
 
-  bandera:boolean=true;
-
-  constructor() { }
+  constructor(private firebaseService:FirebaseService, private router:Router, public auth:Auth) {}
 
   ngOnInit(): void {
+    this.llamadaBandera();
   }
+  // andres@correo.com
 
-  myFunc(){
-    if(this.bandera){
-      this.bandera = false;
-    }else{
+  llamadaBandera(){
+    //muestra u oculta la opcion de iniciar/cerrar sesion y opciones extra
+    if (this.auth.currentUser == null) {
       this.bandera = true;
+    } else {
+      this.bandera = false;
     }
   }
 
+
+  onClick() {
+    this.firebaseService.logout().then(() => {
+      this.llamadaBandera();
+      this.router.navigate(['/home']);
+    });
+  }
 }
