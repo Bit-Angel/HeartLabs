c40850b700b18bd439a7f62e3eec09f1a699d239
brayan <brayanvazq1608@gmail.com>
2022-06-04 13:39:37 -0500

    agregue estilos de nav y animaciones aos

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { fromEvent } from 'rxjs';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  transparenteBool: boolean = true;
+  x = fromEvent(document, 'scroll');
+  otherScroll = window.pageYOffset;
+
+  constructor() {
+    this.x.subscribe( (resp: any) => {
+      const scroll = resp.target.documentElement.scrollTop;
+
+      if(scroll < 80){
+        this.transparenteBool = false;
+      }
+
+      if(scroll < this.otherScroll){
+        this.transparenteBool = true;
+      }
+
+      this.otherScroll = scroll;
+    });
+  }
 
   ngOnInit(): void {
+    AOS.init();
+    window.addEventListener('load', AOS.refresh);
   }
 
 }
