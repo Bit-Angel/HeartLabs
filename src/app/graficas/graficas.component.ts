b8dff79cefa2a6540a35c697f63df51673d06d9e
fatimajcm <fatimamacias500@outlook.com>
2022-06-16 16:38:32 -0500

    2parte: ChartJs en root

--- src/app/graficas/graficas.component.ts
+++ src/app/graficas/graficas.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ChartDataset, Color, ChartType } from 'chart.js';
 import { BaseChartDirective } from 'ng2-charts';
+import { EstudiosService } from '../services/estudios.service';
+import { FirebaseService } from '../services/firebase.service';
+import User from '../interfaces/user.interface';
+import { Router } from '@angular/router';
+import { Auth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-graficas',
@@ -23,30 +28,56 @@ export class GraficasComponent implements OnInit {
   barChartType: ChartType = 'bar';
 
 
-
-  constructor() { }
+  email:any="";
+  phone:any="";
+  usuarioActual:User|any={ //Esta variable guarda todos los datos que estan en la base de datos del usuario que se conecta
+    name:"",  
+    lastName:"",
+    email:"",
+    password:"",
+    phone:"",
+    birthday:""
+  };
+  citas:any[]=[];
+  
+  constructor(public miServicio:EstudiosService,private firebaseService:FirebaseService, private router:Router, public auth:Auth) { }
 
   ngOnInit(): void {
+    this.email=this.auth.currentUser?.email; //Obtener el correo del usuario actual, con el cual podemos obtener el resto de datos
+    this.firebaseService.getUser(this.email)
+    .then(response => {
+      response.forEach((doc) => {
+        this.usuarioActual = doc.data();
+        this.obtenerCitas();
+      });
+    })
+    .catch(error => console.log(error));
+
+    console.log(this.auth.currentUser?.phoneNumber);
+    this.phone = this.auth.currentUser?.phoneNumber;
+    this.firebaseService.getUserPhone(this.phone)
+    .then(response => {
+      response.forEach((doc) => {
+        this.usuarioActual = doc.data();
+        console.log(this.usuarioActual)
+        this.obtenerCitas();
+      });
+    })
+    .catch(error => console.log(error));
+
+
   }
 
-  refresh(){
-
-    this.barChartData = [
-      { data: [Math.random() * (100 - 0) + 0,
-        Math.random() * (100 - 0) + 0,
-        Math.random() * (100 - 0) + 0,
-        Math.random() * (100 - 0) + 0,
-        Math.random() * (100 - 0) + 0,
-        Math.random() * (100 - 0) + 0,
-      ], label: 'Estudios con Doctor' },
-      { data: [Math.random() * (100 - 0) + 0, 
-        Math.random() * (100 - 0) + 0, 
-        Math.random() * (100 - 0) + 0, 
-        Math.random() * (100 - 0) + 0, 
-        Math.random() * (100 - 0) + 0,
-        Math.random() * (100 - 0) + 0
-      ], label: 'Estudios sin Doctor' },
-    ];
+  obtenerCitas(){
+    console.log(this.usuarioActual.email);
+    this.firebaseService.getCitas(this.usuarioActual.email)
+    .then(response => {
+      response.forEach((cita) => {
+        this.citas.push(cita.data())
+        console.log(this.citas);
+      });
+    })
+    .catch(error => console.log(error));
   }
 
 }
