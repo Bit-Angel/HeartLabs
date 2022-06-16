b2a750a7028b405917b59e46925232587ef09d0e
obedphdz <bedphdz@gmail.com>
2022-06-16 09:43:02 -0500

    Preguntas frecuentes y Conocenos(SNJ)

---
+++ src/app/conocenos/mailer.ts
@@ -0,0 +1,15 @@
+// import nodemailer = require('nodemailer');
+
+// export const transporter = nodemailer.createTransport({
+//     host: "smtp.gmail.com",
+//     port: 465,
+//     secure: true, // true for 465, false for other ports
+//     auth: {
+//       user: 'biop60@gmail.com', // generated ethereal user
+//       pass: testAccount.pass, // generated ethereal password
+//     },
+//   });   
+
+//   transporter.verify().then(()=>{
+//     console.log('Listo para enviar correos');
+//   })
